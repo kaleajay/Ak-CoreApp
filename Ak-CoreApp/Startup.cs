9182e089873d4e457b0a04f808a73ba20533df70
kalea <ajay.kale@pillsburylaw.com>
2022-02-22 11:19:44 -0600

    Added Comment

--- Ak-CoreApp/Startup.cs
+++ Ak-CoreApp/Startup.cs
@@ -26,6 +26,7 @@ namespace Ak_CoreApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //Ak - added comment
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
